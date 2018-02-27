fb71c651cc942ff8b9b91338328db1ddd66eac6c
asdflalatran <iamdannyxd@gmail.com>
2018-02-26 20:36:45 -0800

    Update gameplay.java

--- app/src/main/java/com/example/michaelchheang/mathfortoys/gameplay.java
+++ app/src/main/java/com/example/michaelchheang/mathfortoys/gameplay.java
@@ -1,57 +1,59 @@
-package com.example.michaelchheang.mathfortoys;
+package com.example.nick.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.view.View;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
-public class gameplay extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity {
 
     private QuestionLibrary mQuestionLibrary = new QuestionLibrary();
+
     private TextView mScoreView;
     private TextView mQuestionView;
     private Button mButtonChoice1;
     private Button mButtonChoice2;
     private Button mButtonChoice3;
     private Button mButtonChoice4;
-    private ArrayList<Button> buttonList = new ArrayList<Button>();
+    private ArrayList<Button> buttons = new ArrayList<>();
+
     private String mAnswer;
     private int mScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gameplay);
+        setContentView(R.layout.activity_main);
 
-        mScoreView = (TextView) findViewById(R.id.scoreText);
-        mQuestionView = (TextView) findViewById(R.id.problemText);
-        mButtonChoice1 = (Button) findViewById(R.id.choice1_button);
-        mButtonChoice2 = (Button) findViewById(R.id.choice2_button);
-        mButtonChoice3 = (Button) findViewById(R.id.choice3_button);
-        mButtonChoice4 = (Button) findViewById(R.id.choice4_button);
-        buttonList.add(mButtonChoice1);
-        buttonList.add(mButtonChoice2);
-        buttonList.add(mButtonChoice3);
-        buttonList.add(mButtonChoice4);
+        mScoreView = (TextView) findViewById(R.id.scorenum);
+        mQuestionView = (TextView) findViewById(R.id.question);
+        mButtonChoice1 = (Button) findViewById(R.id.choice1);
+        mButtonChoice2 = (Button) findViewById(R.id.choice2);
+        mButtonChoice3 = (Button) findViewById(R.id.choice3);
+        mButtonChoice4 = (Button) findViewById(R.id.choice4);
+
+        buttons.add(mButtonChoice1);
+        buttons.add(mButtonChoice2);
+        buttons.add(mButtonChoice3);
+        buttons.add(mButtonChoice4);
 
         updateQuestion();
+
         mButtonChoice1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (mButtonChoice1.getText() == mAnswer) {
                     mScore++;
                     updateScore(mScore);
-                    Toast.makeText(gameplay.this, "correct", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "correct", Toast.LENGTH_SHORT).show();
                     updateQuestion();
                 } else {
-                    Toast.makeText(gameplay.this, "wrong", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "wrong", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -61,10 +63,10 @@ public class gameplay extends AppCompatActivity {
                 if (mButtonChoice2.getText() == mAnswer) {
                     mScore++;
                     updateScore(mScore);
-                    Toast.makeText(gameplay.this, "correct", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "correct", Toast.LENGTH_SHORT).show();
                     updateQuestion();
                 } else {
-                    Toast.makeText(gameplay.this, "wrong", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "wrong", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -74,10 +76,10 @@ public class gameplay extends AppCompatActivity {
                 if (mButtonChoice3.getText() == mAnswer) {
                     mScore = mScore + 1;
                     updateScore(mScore);
-                    Toast.makeText(gameplay.this, "correct", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "correct", Toast.LENGTH_SHORT).show();
                     updateQuestion();
                 } else {
-                    Toast.makeText(gameplay.this, "wrong", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "wrong", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -87,27 +89,26 @@ public class gameplay extends AppCompatActivity {
                 if (mButtonChoice4.getText() == mAnswer) {
                     mScore++;
                     updateScore(mScore);
-                    Toast.makeText(gameplay.this, "correct", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "correct", Toast.LENGTH_SHORT).show();
                     updateQuestion();
                 } else {
-                    Toast.makeText(gameplay.this, "wrong", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "wrong", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
-
-    private void updateQuestion() {
+    private void updateQuestion(){
         mQuestionView.setText(mQuestionLibrary.getKQuestion());
-        Collections.shuffle(buttonList);
-        buttonList.get(0).setText(String.valueOf(mQuestionLibrary.getChoice1()));
-        buttonList.get(1).setText(String.valueOf(mQuestionLibrary.getChoice2()));
-        buttonList.get(2).setText(String.valueOf(mQuestionLibrary.getChoice3()));
-        buttonList.get(3).setText(String.valueOf(mQuestionLibrary.getChoice4()));
-
+        Collections.shuffle(buttons);
         mAnswer = String.valueOf(mQuestionLibrary.getAnswer());
+        buttons.get(0).setText(mAnswer);
+        for (int i = 1; i < buttons.size(); i++){
+            buttons.get(i).setText(String.valueOf(mQuestionLibrary.getChoice(i)));
+        }
     }
 
     private void updateScore(int mScore) {
         mScoreView.setText("" + mScore);
     }
 }
+
